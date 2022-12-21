a8d2ee36c73e033269e941d8d1be0d988864a5fd
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2022-12-21 11:32:25 +0000

    2

--- parte2(2).py
+++ parte2(2).py
@@ -16,3 +16,38 @@ mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255
 #If this page is executed with no error, you have successfully created a table named "customers".
 
 #Puoi controllare se esiste una tabella elencando tutte le tabelle nel tuo database # istruzione "SHOW TABLES":   # mycursor.execute("SHOW TABLES")
+
+# CHIAVE PRIMARIA
+#Quando crei una tabella, dovresti anche creare una colonna con una chiave univoca
+#'istruzione "INT AUTO_INCREMENT PRIMARY KEY" che inserirà un numero univoco per ogni record. A partire da 1 e aumentato di uno per ogni record.
+#record = 
+
+#Crea chiave primaria durante la creazione della tabella:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+#Se la tabella esiste già, utilizzare la parola chiave ALTER TABLE:
+
+#Crea chiave primaria su una tabella esistente:
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
