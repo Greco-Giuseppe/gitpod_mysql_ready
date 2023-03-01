03344fddc43314bb32e0ea1ba9379926df78da68
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2023-03-01 11:51:33 +0000

    1 marzo

--- a.py
+++ a.py
@@ -2,11 +2,18 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="myusername",
-  password="mypassword",
+  user="root",
+  password="",
   database="prova3"
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE tabella (name VARCHAR(255), address VARCHAR(255))")
+sql = "INSERT INTO tabella (name, address) VALUES (%s , %s)"    #%s stringa  %d intero    #name address  in alto alla tabella eexit
+val = ("Giuseppe" , "Milano 18")
+
+mycursor.execute(sql, val)
+
+mydb.commit() # apporta le modifiche
+
+print(mycursor.rowcount , "inserito")
