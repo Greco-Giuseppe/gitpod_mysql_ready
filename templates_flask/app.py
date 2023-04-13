69cc0c527df89d31fd016bf4e8df0041ea23b10a
Greco-Giuseppe <greco.giuseppe@iisgalvanimi.edu.it>
2023-04-13 10:48:42 +0000

    13aprile

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -1,10 +1,27 @@
+import mysql.connector
+
 from flask import render_template
 from flask import Flask
 
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="CLASH_ROYALE"
+)
+mycursor = mydb.cursor()
+
 
 @app.route('/')
 def hello():
     return render_template('hello.html', name='Giuseppe')
+
+
+@app.route('/units')
+def unitlist():
+    mycursor.execute("SELECT* FROM Clash_Unit")
+    myresult = mycursor.fetchall()
+    return render_template('clash_units.html' , units=myresult)
